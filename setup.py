3c235f7b126a21979596d423dd84db90a457365d
Denis 'jawa' Pompilio <denis.pompilio@nbs-system.com>
2017-02-22 14:38:12 +0100

    Bump to version 0.9.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from distutils.core import setup
 
 if __name__ == '__main__':
     readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
-    release = "0.9.0"
+    release = "0.9.1"
 
     setup(
         name="python-pylls",
